edcb9ed52d7c8d8ca5b598b84c0d2762dcff0608
Arturo Ruiz <yo130900@gmail.com>
2023-06-17 06:01:03 -0600

    Update

---
+++ src/app/graficas/graficas.component.ts
@@ -0,0 +1,12 @@
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import Chart from 'chart.js/auto';
+
+
+@Component({
+  selector: 'app-graficas',
+  templateUrl: './graficas.component.html',
+  styleUrls: ['./graficas.component.css']
+})
+export class GraficasComponent {
+  
+}
